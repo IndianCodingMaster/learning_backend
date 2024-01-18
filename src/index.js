45b7c336b64ac796a644ca1bbf963cfd85be2072
Satya Prakash <satyaprakash91180.com>
2024-01-18 23:25:07 +0530

    apply changes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
-import dotenv from "dotenv";
-import express from "express";
-import connectDB from "../src/db/connectdb.js";
-
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
 dotenv.config({
-  path: "./.env",
-});
+    path: './.env'
+})
 
-const app = express();
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running at ${process.env.PORT}`);
-  connectDB();
-});
+
+connectDB()
+
